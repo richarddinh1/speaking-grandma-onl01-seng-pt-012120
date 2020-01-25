1974c66801648bbff5d6b580e435b680c482f1f5
richard dinh <richardndinh@gmail.com>
2020-01-25 03:11:39 +0000

    Done.

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -20,3 +20,13 @@ describe '#speak_to_grandma' do
   end
 
 end
+
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    elsif string != string.upcase
+      return "HUH?! SPEAK UP, SONNY!"
+    elsif string == string.upcase
+      return "NO, NOT SINCE 1938!"
+   end 
+end
