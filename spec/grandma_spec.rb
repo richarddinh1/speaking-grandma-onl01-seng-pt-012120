7a4fec997856db1c9ade5a37950a985d88f6d28a
richard dinh <richardndinh@gmail.com>
2020-01-25 03:05:29 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -27,6 +27,6 @@ def speak_to_grandma(string)
     elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
     elsif string != string.upcase
-    return "NO, NOT SINCE 19"
+    return "NO, NOT SINCE 1938!"
    end 
 end
