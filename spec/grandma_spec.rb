9d9cb0f990ee5ee0a48c532e6831f912d53673b7
richard dinh <richardndinh@gmail.com>
2020-01-25 03:10:36 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -23,6 +23,7 @@ end
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
     return "NO, NOT SINCE 1938!"
     elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
